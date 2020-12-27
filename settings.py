29b4d3986685c8327421528ccda00a3c8366ed85
Andrea Coi <andrea@devbook.local>
2020-12-27 13:26:32 +0100

    Creazione e gestione di logfolder

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@ from pathlib import Path
 baudrate = 1200
 path = '/dev/ttyUSB0'
 parity = 'PARITY_EVEN'
-logfolder = '/archivio/triride/'
+logfolder = '/archivio/triride/' #passaggio della directory da riga di comando ??? da fare?
 home = str(Path.home())
 logfolder = home + logfolder
 timeout = 0

f7c28ea7b1b58cb79d029c0cf5544de191f1de35
Andrea Coi <andrea@devbook.local>
2020-12-27 13:24:26 +0100

    Demandata totalmente a settings la creazione di logfolder

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import os
 import communication
 import settings
-from pathlib import Path
+
 
 def readresults(name):
 
     print(f'Hi, {name}')
-    home = str(Path.home())
-    logfile = open(home + settings.logfolder + 'logfile.txt', 'w+')
+
+    logfile = open(settings.logfolder + 'logfile.txt', 'w+')
 
     logfile.write(communication.results)
 
